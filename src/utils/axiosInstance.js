d4c668e6ba0b63682297851eb3ff3b212262a64e
Tùng Lâm Hà <hatunglambg2003@gmail.com>
2025-03-24 11:33:35 +0700

    Pull request

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import { message } from 'antd';
-const API_URL = 'http://192.168.100.4:2003/api/';
+const API_URL = 'http://192.168.100.56:2003/api/';
 // const API_URL = 'https://localhost:7024/api/';
 const axiosInstance = axios.create({
   baseURL: API_URL,
